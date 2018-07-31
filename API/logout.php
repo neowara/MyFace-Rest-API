b1a4b91cd7a4b43ef867ffed5ef52f5b25f10ec2
Christian <32764514+neowara@users.noreply.github.com>
2018-08-01 00:30:57 +0200

    Cleaned up and added icon to tab

--- API/logout.php
+++ API/logout.php
@@ -12,17 +12,17 @@ session_destroy();
 <head>
     <meta charset="utf-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>Page Title</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
-      <link rel="stylesheet" type="text/css" href="../CSS/style.css"/>
-        <link href="https://fonts.googleapis.com/css?family=Exo+2" rel="stylesheet">
-    <script src="main.js"></script>
+    <link rel="stylesheet" type="text/css" href="../CSS/style.css"/>
+    <link href="https://fonts.googleapis.com/css?family=Exo+2" rel="stylesheet">
+    <link rel="icon" href="http://mariadesiree.se/christiantoledo/MyFace_API/favicon.ico">
+    <title>MyFace</title>
 </head>
 <body>
 
 <div id="logoutPage">
 <p>Session Cleared!</p>
-<button id="logoutButton"><a href="../index.html" id="logoutLink">Go Back</a></button>
+<a href="../index.html" id="logoutLink"><button id="logoutButton">Go Back</button></a>
 </div>
 
 </body>
